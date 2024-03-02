6ecbe0ba3c33a0e5907b2a143afe94e261deeb4d
Sangita <sangita700035@gmail.com>
2024-03-03 01:17:54 +0530

    Added useful links

---
+++ script.js
@@ -0,0 +1,147 @@
+//==================  toggle icon navbar ============================
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+//==================  scroll sections active link ============================
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+        };
+    });
+    //==================  sticky navbar ============================
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //================== remove navbar and toggle icon when click navbar link ============================
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+//==================  scroll reveal ============================
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer', 'Frontend Developer', '.Net Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+const form = document.querySelector("form");
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullname.value}<br> Email: ${email.value}<br>
+    Mobile Number: ${number.value}<br>
+    Message: ${message.value} `;
+    Email.send({
+        SecureToken: "e2946f3c-16c0-4da5-8108-5be107c570a1",
+        To: "700035sangita@gmail.com",
+        From: "700035sangita@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    icon: "success",
+                    title: "Message Sent Successfully!"
+                });
+            }
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+    for (const item of items) {
+
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+        if (items[1].value != "") {
+            checkEmail()
+        }
+        items[1].addEventListener("keyup", () => {
+            checkEmail()
+        })
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            } else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    const errorTextEmail = document.querySelector(".error-text.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+        if (email.value != "") {
+            errorTextEmail.innerHTML = "Enter a valid Email Address!";
+        }
+        else {
+            errorTextEmail.innerHTML = "Email Address can't be blank!";
+        }
+    } else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullname.classList.contains("error") && !email.classList.contains("error") &&
+        !number.classList.contains("error") && !subject.classList.contains("error") &&
+        !message.classList.contains("error")) {
+
+        sendEmail();
+        form.reset();
+        return false;
+    }
+
+})
