740cea4df4317797eae6c795ba75186c72261dc9
Sangita <sangita700035@gmail.com>
2024-03-18 16:00:29 +0530

    RTU

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'Frontend Developer', '.Net Developer'],
+    strings: ['Frontend Developer', 'Web Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
